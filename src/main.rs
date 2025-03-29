b92bbc5481a236bbf2bcf6fbaa1a4637a51a5fb0
尤雨东 <root@ltpp.vip>
2025-03-29 20:23:48 +0800

    feat: v0.0.5

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,14 @@ fn get_json_string(code: usize, msg: &str, data: &str) -> String {
     )
 }
 
-async fn resp_json(
-    arc_lock_controller_data: &ControllerData,
-    content_type: &str,
-    code: usize,
-    msg: &str,
-    data: &str,
-) {
+async fn resp_json(ctx: &Context, content_type: &str, code: usize, msg: &str, data: &str) {
     let json_string: String = get_json_string(code, msg, data);
-    let host: String = arc_lock_controller_data
+    let host: String = ctx
         .get_socket_addr()
         .await
         .unwrap_or(DEFAULT_SOCKET_ADDR)
         .to_string();
-    let _ = arc_lock_controller_data
+    let _ = ctx
         .set_response_header(ACCESS_CONTROL_ALLOW_ORIGIN, ANY)
         .await
         .set_response_header(ACCESS_CONTROL_ALLOW_METHODS, GET_POST_OPTIONS)
@@ -72,26 +66,20 @@ async fn resp_json(
         .await
         .send_response(200, json_string.clone())
         .await;
-    arc_lock_controller_data
-        .log_info(
-            format!("{} resp_json => {}", host, json_string),
-            log_handler,
-        )
-        .await;
+    ctx.log_info(
+        &format!("{} resp_json => {}", host, json_string),
+        log_handler,
+    )
+    .await;
 }
 
-async fn resp_bin(
-    arc_lock_controller_data: &ControllerData,
-    status_code: usize,
-    content_type: &str,
-    data: Vec<u8>,
-) {
-    let host: String = arc_lock_controller_data
+async fn resp_bin(ctx: &Context, status_code: usize, content_type: &str, data: Vec<u8>) {
+    let host: String = ctx
         .get_socket_addr()
         .await
         .unwrap_or(DEFAULT_SOCKET_ADDR)
         .to_string();
-    let _ = arc_lock_controller_data
+    let _ = ctx
         .set_response_header(ACCESS_CONTROL_ALLOW_ORIGIN, ANY)
         .await
         .set_response_header(ACCESS_CONTROL_ALLOW_METHODS, GET_POST_OPTIONS)
@@ -102,44 +90,29 @@ async fn resp_bin(
         .await
         .send_response(status_code, data)
         .await;
-    arc_lock_controller_data
-        .log_info(
-            format!(
-                "{} resp_bin => content_type:{} status_code:{}",
-                host, content_type, status_code
-            ),
-            log_handler,
-        )
-        .await;
+    ctx.log_info(
+        &format!(
+            "{} resp_bin => content_type:{} status_code:{}",
+            host, content_type, status_code
+        ),
+        log_handler,
+    )
+    .await;
 }
 
-async fn success_resp_json(
-    arc_lock_controller_data: &ControllerData,
-    content_type: &str,
-    msg: &str,
-    data: &str,
-) {
+async fn success_resp_json(ctx: &Context, content_type: &str, msg: &str, data: &str) {
     println_success!("success_resp_json", " => ", msg, " ", data);
-    resp_json(arc_lock_controller_data, content_type, 1, msg, data).await;
+    resp_json(ctx, content_type, 1, msg, data).await;
 }
 
-async fn error_resp_json(
-    arc_lock_controller_data: &ControllerData,
-    content_type: &str,
-    msg: &str,
-    data: &str,
-) {
+async fn error_resp_json(ctx: &Context, content_type: &str, msg: &str, data: &str) {
     println_error!("error_resp_json", " => ", msg, " ", data);
-    resp_json(arc_lock_controller_data, content_type, 0, msg, data).await;
+    resp_json(ctx, content_type, 0, msg, data).await;
 }
 
-async fn success_resp_bin(
-    arc_lock_controller_data: &ControllerData,
-    content_type: &str,
-    data: Vec<u8>,
-) {
+async fn success_resp_bin(ctx: &Context, content_type: &str, data: Vec<u8>) {
     println_success!("success_resp_bin", " => ", content_type);
-    resp_bin(arc_lock_controller_data, 200, content_type, data).await;
+    resp_bin(ctx, 200, content_type, data).await;
 }
 
 fn encode_file_full_path(path: &str) -> String {
@@ -152,8 +125,8 @@ fn decode_file_full_path(path: &str) -> String {
     format!("{}.{}", decode(CHARSET, prefix).unwrap(), suffix)
 }
 
-async fn file_middleware(arc_lock_controller_data: ControllerData) {
-    let controller_data: InnerControllerData = arc_lock_controller_data.get().await;
+async fn file_middleware(ctx: Context) {
+    let controller_data: InnerContext = ctx.get().await;
     let path: &String = controller_data.get_request().get_path();
     let extension_name: String = FileExtension::get_extension_name(path);
     let content_type: &str = FileExtension::parse(&extension_name).get_content_type();
@@ -165,29 +138,23 @@ async fn file_middleware(arc_lock_controller_data: ControllerData) {
         return;
     }
     if file_path.contains("../") {
-        return error_resp_json(
-            &arc_lock_controller_data,
-            content_type,
-            &format!("{} unsafe", FILE_NAME_KEY),
-            "",
-        )
-        .await;
+        return error_resp_json(&ctx, content_type, &format!("{} unsafe", FILE_NAME_KEY), "").await;
     }
     let body: Vec<u8> = async_read_from_file(&file_path).await.unwrap();
-    success_resp_bin(&arc_lock_controller_data, content_type, body).await;
+    success_resp_bin(&ctx, content_type, body).await;
 }
 
-async fn index_file(arc_lock_controller_data: ControllerData) {
-    let _ = arc_lock_controller_data.send_response(200, FILE).await;
+async fn index_file(ctx: Context) {
+    let _ = ctx.send_response(200, FILE).await;
 }
 
-async fn add_file(arc_lock_controller_data: ControllerData) {
-    let controller_data: InnerControllerData = arc_lock_controller_data.get().await;
+async fn add_file(ctx: Context) {
+    let controller_data: InnerContext = ctx.get().await;
     let req: &Request = controller_data.get_request();
     let file_name_opt: Option<String> = req.get_query(FILE_NAME_KEY);
     if file_name_opt.is_none() {
         return error_resp_json(
-            &arc_lock_controller_data,
+            &ctx,
             APPLICATION_JSON,
             &format!("missing {}", FILE_NAME_KEY),
             "",
@@ -198,7 +165,7 @@ async fn add_file(arc_lock_controller_data: ControllerData) {
     let file_name_path: String = format!("/{}", file_name);
     if file_name_path.contains("../") {
         return error_resp_json(
-            &arc_lock_controller_data,
+            &ctx,
             APPLICATION_JSON,
             &format!("{} unsafe", FILE_NAME_KEY),
             "",
@@ -208,28 +175,16 @@ async fn add_file(arc_lock_controller_data: ControllerData) {
     let extension_name: String = FileExtension::get_extension_name(&file_name_path);
     let content_type: &str = FileExtension::parse(&extension_name).get_content_type();
     if content_type.is_empty() {
-        return error_resp_json(
-            &arc_lock_controller_data,
-            APPLICATION_JSON,
-            "file type not supported",
-            "",
-        )
-        .await;
+        return error_resp_json(&ctx, APPLICATION_JSON, "file type not supported", "").await;
     }
     let file_data: &Vec<u8> = req.get_body();
     let file_data_len: usize = file_data.len();
     if file_data_len == 0 {
-        return error_resp_json(
-            &arc_lock_controller_data,
-            APPLICATION_JSON,
-            "file can not empty",
-            "",
-        )
-        .await;
+        return error_resp_json(&ctx, APPLICATION_JSON, "file can not empty", "").await;
     }
     if file_data_len > FILE_MAX_SIZE {
         return error_resp_json(
-            &arc_lock_controller_data,
+            &ctx,
             APPLICATION_JSON,
             &format!("file size over {} bytes", FILE_MAX_SIZE),
             "",
@@ -238,47 +193,25 @@ async fn add_file(arc_lock_controller_data: ControllerData) {
     }
     let full_path: String = get_file_full_path(&file_name_path).await;
     if metadata(&full_path).await.is_ok() {
-        return error_resp_json(
-            &arc_lock_controller_data,
-            APPLICATION_JSON,
-            "file already exist",
-            "",
-        )
-        .await;
+        return error_resp_json(&ctx, APPLICATION_JSON, "file already exist", "").await;
     }
     let write_res: Result<(), std::io::Error> = async_write_to_file(&full_path, file_data).await;
     if let Err(err) = write_res {
-        return error_resp_json(
-            &arc_lock_controller_data,
-            APPLICATION_JSON,
-            &format!("{:?}", err),
-            "",
-        )
-        .await;
+        return error_resp_json(&ctx, APPLICATION_JSON, &format!("{:?}", err), "").await;
     }
     let encode_full_path: String = encode_file_full_path(&full_path);
     let encode_full_url: String = format!("{}/{}", PROTOCOL_DOMAIN_NAME, encode_full_path);
-    success_resp_json(
-        &arc_lock_controller_data,
-        APPLICATION_JSON,
-        "ok",
-        &encode_full_url,
-    )
-    .await;
+    success_resp_json(&ctx, APPLICATION_JSON, "ok", &encode_full_url).await;
 }
 
-async fn run_server() {
-    let mut server: Server = Server::new();
+#[tokio::main]
+async fn main() {
+    let server: Server = Server::new();
     server.host("0.0.0.0").await;
     server.port(60006).await;
     server.log_dir(LOG_DIR).await;
     server.request_middleware(file_middleware).await;
     server.route("/", index_file).await;
     server.route("/add_file", add_file).await;
-    server.listen().await;
-}
-
-#[tokio::main]
-async fn main() {
-    run_server().await;
+    server.listen().await.unwrap();
 }
